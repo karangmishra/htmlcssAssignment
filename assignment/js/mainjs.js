1b206b3d3d2764a876eb96bb0c1ed7ae9f95ec84
karangmishra <karangmishra@gmail.com>
2020-12-23 14:30:11 +0530

    faq section

--- assignment/js/mainjs.js
+++ assignment/js/mainjs.js
@@ -1,6 +1,4 @@
-/* to make a button  
- * on click goes to top
- */
+/* to make a button   on click goes to top*/
 let topbtn = document.getElementById("top-btn");
 window.onscroll = function() {
     scrollFunction()
@@ -20,4 +18,14 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+/*show answer of FAQ section */
+function show() {
+    let getData = document.getElementById("hide-content");
+    if (getData.style.display === "none") {
+        getData.style.display = "block";
+    } else {
+        getData.style.display = "none"
+    }
 }
