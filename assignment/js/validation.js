f4c5ab21f98d30fad1e129e1ce65c81941080f12
karangmishra <karangmishra@gmail.com>
2021-01-22 19:14:03 +0530

    remove unwanted file and update project

--- assignment/js/validation.js
+++ assignment/js/validation.js
@@ -4,7 +4,7 @@ function formValidation() {
   let subject = document.registration.subject;
   let cmessage = document.registration.message;
   let email = document.registration.email;
-  var text = "";
+  let text = "";
 
   if (useridValidation(uid)) {
     if (validSubject(subject)) {
@@ -18,7 +18,7 @@ function formValidation() {
 }
 
 function useridValidation(uid) {
-  var message = document.getElementsByClassName("error-message");
+  let message = document.getElementsByClassName("error-message");
   let uid_len = uid.value.length;
   let letters = /^[A-Za-z]+[ A-Za-z]*$/;
   if (uid_len == !0 || uid_len >= 3 && uid_len < 20 && uid.value.match(letters)) {
@@ -27,7 +27,7 @@ function useridValidation(uid) {
     return true;
   }
   uid.focus();
- /* alert("name should be minimum 3 character and maximum 20");*/
+  /* alert("name should be minimum 3 character and maximum 20");*/
   text = "name should contain minimum 3 letters and maximum 20";
   message[0].innerHTML = text;
   uid.focus();
@@ -35,7 +35,7 @@ function useridValidation(uid) {
 }
 
 function validSubject(subject) {
-  var message = document.getElementsByClassName("error-message");
+  let message = document.getElementsByClassName("error-message");
   let letters = /^[A-Za-z0-9-]+[ 0-9A-Za-z#$%=@!{},`~&*()'<>?.:;_|^/+\t\r\n\[\]"-]*$/;
   let sub_leng = subject.value.length;
 
@@ -44,7 +44,7 @@ function validSubject(subject) {
     message[2].innerHTML = text;
     return true;
   } else {
-   /* alert("subject should be minimum 5 character and maximum 80");*/
+    /* alert("subject should be minimum 5 character and maximum 80");*/
     text = "subject should be minimum 5 character and maximum 80";
     message[2].innerHTML = text;
     subject.focus();
@@ -53,8 +53,8 @@ function validSubject(subject) {
 }
 
 function validMessage(cmessage) {
-  var message = document.getElementsByClassName("error-message");
-  let letters =/^[A-Za-z0-9-]+[ 0-9A-Za-z#$%=@!{},`~&*()'<>?.:;_|^/+\t\r\n\[\]"-]*$/;
+  let message = document.getElementsByClassName("error-message");
+  let letters = /^[A-Za-z0-9-]+[ 0-9A-Za-z#$%=@!{},`~&*()'<>?.:;_|^/+\t\r\n\[\]"-]*$/;
   let message_leng = cmessage.value.length;
 
   if (cmessage.value.match(letters) && message_leng > 4 && message_leng < 80) {
@@ -62,7 +62,7 @@ function validMessage(cmessage) {
     message[3].innerHTML = text;
     return true;
   } else {
-   /* alert("message should be minimum 5 character and maximum 80");*/
+    /* alert("message should be minimum 5 character and maximum 80");*/
     text = "message should be minimum 5 character and maximum 80";
     message[3].innerHTML = text;
     cmessage.focus();
@@ -72,7 +72,7 @@ function validMessage(cmessage) {
 
 /* validate email */
 function validateEmail(email) {
-  var message = document.getElementsByClassName("error-message");
+  let message = document.getElementsByClassName("error-message");
   let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   if (email.value.match(mailformat)) {
     text = "";
@@ -80,7 +80,7 @@ function validateEmail(email) {
     window.location.href = './html/thanks.html';
     return true;
   } else {
-  /* alert("email required example:- abc@gmail.com")*/
+    /* alert("email required example:- abc@gmail.com")*/
     text = "email required example:- abc@gmail.com";
     message[1].innerHTML = text;
     email.focus();
@@ -90,23 +90,20 @@ function validateEmail(email) {
 
 document.addEventListener('DOMContentLoaded', function () {
   /* everything else we type will go inside this!! */
-
-  const bookContainer = document.querySelector('#book-container')
-  const bookURL = `http://localhost:3000/users`
-  const bookForm = document.querySelector('#book-form')
-  var d = new Date();
+  const bookURL = `http://localhost:3000/users`;
+  const bookForm = document.querySelector('#book-form');
+  let d = new Date();
 
   fetch(`${bookURL}`)
     .then(response => response.json())
-    
-   
+
   bookForm.addEventListener('submit', (e) => {
     e.preventDefault()
     /* additional functionality goes down here!!*/
-    const usernameInput = bookForm.querySelector('#username').value
-    const emailInput = bookForm.querySelector('#email').value
-    const messageInput = bookForm.querySelector('#message').value
-    const subjectInput = bookForm.querySelector('#subject').value
+    const usernameInput = bookForm.querySelector('#username').value;
+    const emailInput = bookForm.querySelector('#email').value;
+    const messageInput = bookForm.querySelector('#message').value;
+    const subjectInput = bookForm.querySelector('#subject').value;
 
     fetch(`${bookURL}`, {
       method: 'POST',
@@ -115,12 +112,12 @@ document.addEventListener('DOMContentLoaded', function () {
         email: emailInput,
         subject: subjectInput,
         message: messageInput,
-        date: d,
+        date: d
       }),
+
       headers: {
         'Content-Type': 'application/json'
       }
     }).then(response => response.json())
-  
   })
 })
