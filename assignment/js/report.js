4a0d24d5bf3008d584b3c792a2bdb13bd91ecb7f
karangmishra <karangmishra@gmail.com>
2021-02-16 13:55:03 +0530

    js w3 standard

--- assignment/js/report.js
+++ assignment/js/report.js
@@ -1,10 +1,10 @@
 /*reading a file is an async operation.*/
 function readTextFile(file, callback) {
   var rawFile = new XMLHttpRequest();
-  rawFile.overrideMimeType("application/json");
-  rawFile.open("GET", file, true);
+  rawFile.overrideMimeType('application/json');
+  rawFile.open('GET', file, true);
   rawFile.onreadystatechange = function () {
-    if (rawFile.readyState === 4 && rawFile.status == "200") {
+    if (rawFile.readyState === 4 && rawFile.status == '200') {
       callback(rawFile.responseText);
     }
   }
@@ -13,22 +13,22 @@ function readTextFile(file, callback) {
 
 var showdata = function () {
   return new Promise(function (resolve, reject) {
-    readTextFile("../formdata.json", function (text) {
+    readTextFile('../formdata.json', function (text) {
       var data = JSON.parse(text);
-  
+
       if (data.users.length > 0) {
         objJson = data.users;
-        let temp = "";
+        let temp = '';
         data.users.forEach((d) => {
-          if(d.id < 6){
-          temp += "<tr>";
-          temp += "<td>" + d.id + "</td>";
-          temp += "<td>" + d.username + "</td>";
-          temp += "<td>" + d.date + "</td>";
-          temp += "</tr>";
+          if (d.id < 6) {
+            temp += '<tr>';
+            temp += '<td>' + d.id + '</td>';
+            temp += '<td>' + d.username + '</td>';
+            temp += '<td>' + d.date + '</td>';
+            temp += '</tr>';
           }
         })
-        document.getElementById("report").innerHTML = temp;
+        document.getElementById('report').innerHTML = temp;
       }
       resolve('task1');
     });
@@ -62,32 +62,32 @@ function nextPage() {
 }
 
 function changePage(page) {
-  var btnNext = document.getElementById("btn-next");
-  var btnPrev = document.getElementById("btn-prev");
-  var listingTable = document.getElementById("report");
-  var pageSpan = document.getElementById("page");
+  var btnNext = document.getElementById('btn-next');
+  var btnPrev = document.getElementById('btn-prev');
+  var listingTable = document.getElementById('report');
+  var pageSpan = document.getElementById('page');
 
   // Validate page
   if (page < 1) page = 1;
   if (page > numPages()) page = numPages();
 
-  listingTable.innerHTML = "";
+  listingTable.innerHTML = '';
 
   for (var i = (page - 1) * recordsPerPage; i < (page * recordsPerPage); i++) {
-    listingTable.innerHTML += "<tr><td>" + objJson[i].id + "</td><td>" + objJson[i].username + "</td><td>" + objJson[i].date.split('T')[0] + "</td></tr>";
+    listingTable.innerHTML += '<tr><td>' + objJson[i].id + '</td><td>' + objJson[i].username + '</td><td>' + objJson[i].date.split('T')[0] + '</td></tr>';
   }
   pageSpan.innerHTML = page;
 
   if (page == 1) {
-    btnPrev.style.visibility = "hidden";
+    btnPrev.style.visibility = 'hidden';
   } else {
-    btnPrev.style.visibility = "visible";
+    btnPrev.style.visibility = 'visible';
   }
 
   if (page == numPages()) {
-    btnNext.style.visibility = "hidden";
+    btnNext.style.visibility = 'hidden';
   } else {
-    btnNext.style.visibility = "visible";
+    btnNext.style.visibility = 'visible';
   }
 }
 
